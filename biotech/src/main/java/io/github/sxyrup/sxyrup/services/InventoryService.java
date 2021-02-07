bdf103d39e062dbc77875cf007461ffa57bd5d1c
Dora Ree <daura.ree@gmail.com>
2021-02-07 11:28:18 +0100

    refactor, still messy

--- biotech/src/main/java/io/github/sxyrup/sxyrup/services/InventoryService.java
+++ biotech/src/main/java/io/github/sxyrup/sxyrup/services/InventoryService.java
@@ -2,11 +2,9 @@ package io.github.sxyrup.sxyrup.services;
 
 import io.github.sxyrup.sxyrup.DIConfiguration;
 import io.github.sxyrup.sxyrup.models.Vial;
-import io.github.sxyrup.sxyrup.repositories.CellRepo;
 import io.github.sxyrup.sxyrup.repositories.ICellRepo;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,7 +17,6 @@ public class InventoryService {
     try {
       DIConfiguration diConfiguration = new DIConfiguration();
       this.cellRepo = diConfiguration.buildRecipeRepo();
-      System.out.println("hello");
     } catch (IOException e) {
       e.printStackTrace();
     }
